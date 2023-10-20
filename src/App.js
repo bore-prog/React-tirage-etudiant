65e96e43236aff7d4ba97e1df2ce01259d2756c5
bore-prog <philippeaurore@orange.fr>
2023-10-20 14:18:15 +0200

    INSTALL React Modules, ADD packages files and public directory

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './componants/header/header';
+import Footer from './componants/footer/footer';
+import { MissingPeopleProvider } from './componants/missingPeoples/missingPeopleContext';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
