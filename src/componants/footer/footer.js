741ced5ed0512af608d918e3ae95ae02f3668992
bore-prog <philippeaurore@orange.fr>
2023-10-20 15:24:51 +0200

    FEAT footer

---
+++ src/componants/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            Made with ❤ by <strong><a href='https://github.com/bore-prog'>Aurore PHILIPPE</a></strong>
+        </footer>
+    );
+}
+
+export default Footer;
